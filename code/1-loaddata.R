99986fc07841a5cfdfc070cd5b7a592336443546
Edouard-Legoupil <edouard.legoupil@gmail.com>
2019-02-27 11:15:25 +0200

    add loop

--- code/1-loaddata.R
+++ code/1-loaddata.R
@@ -15,6 +15,10 @@ library(koboloadeR)
 ## kobo_projectinit()
 ## Now Position your form & your data in the data folder
 
+## Small function used later in case a modality is not used
+intersect_all <- function(a,b,...){
+  Reduce(intersect, list(a,b,...))
+}
 
 
 ## Load form and building dictionnary #############################################
@@ -110,10 +114,6 @@ questions$std <- 0
 questions$type <- as.character(questions$type)
 levels(as.factor(questions$type))
 
-## Small function used later in case a modality is not used
-intersect_all <- function(a,b,...){
-  Reduce(intersect, list(a,b,...))
-}
 
 ## initialise score dataframe
 scores <- data.frame(operation = as.character("test"),
@@ -125,7 +125,7 @@ scores <- data.frame(operation = as.character("test"),
 
 
 
-## Now compile indicators for each country
+## Now compile indicators for each country #####
 for (i in 1:nrow(questions)) {
   # i <- 41
   questionname <- as.character(questions[ i, c("fullname")])
@@ -254,259 +254,88 @@ scores.full$test <- (scores.full$score - scores.full$mean) / scores.full$std
 scores.full$newname <- paste0(scores.full$theme, "_", scores.full$name)
 
 
-
+## Backup for report
 write.csv(scores.full,"data/scorefull.csv", row.names = FALSE, na = "")
-
+write.csv(questions,"data/questions.csv", row.names = FALSE, na = "")
 
 levels(as.factor(scores.full$concept))
-#############################################################################
-##### Step : subsetting by concept to check clusters
 
-scores.Readiness <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == "Readiness" , ]
-scores.Readiness.wide <- dcast(scores.Readiness, operation  ~ newname, value.var = "scaled.score", sum)
-row.names(scores.Readiness.wide ) <- scores.Readiness.wide$operation
-scores.Readiness.wide$operation <- NULL
+### Compile for Cronbach alpha #########
+scores.wide <- scores.full[ !is.na(scores.full$scaled.score)  , ]
+scores.wide <- dcast(scores.wide, operation  ~ newname, value.var = "scaled.score", sum)
+row.names(scores.wide ) <- scores.wide$operation
+scores.wide$operation <- NULL
+Cronbach <- alpha(scores.wide, check.keys = TRUE)
 
-scores.Access <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == "Access" , ]
-scores.Access.wide <- dcast(scores.Access, operation  ~ newname, value.var = "scaled.score", sum)
-row.names(scores.Access.wide ) <- scores.Access.wide$operation
-scores.Access.wide$operation <- NULL
 
-scores.Environement <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == "Environement" , ]
-scores.Environement.wide <- dcast(scores.Environement, operation  ~ newname, value.var = "scaled.score", sum)
-row.names(scores.Environement.wide ) <- scores.Environement.wide$operation
-scores.Environement.wide$operation <- NULL
+### Compile summary score per sector
 
-scores.Situation <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == "Situation" , ]
-scores.Situation.wide <- dcast(scores.Situation, operation  ~ newname, value.var = "scaled.score", sum)
-row.names(scores.Situation.wide ) <- scores.Situation.wide$operation
-scores.Situation.wide$operation <- NULL
+themes.op <- as.data.frame(data$intro.Operation)
+themes <- as.data.frame(unique(questions$theme))
+for (i in 1:nrow(themes)) {
+  # i <- 2
+  theme.this <- as.character(themes[ i, 1])
+  ## subset
+  scores.this <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$theme == theme.this , ]
 
-scores.IDP <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == "IDP" , ]
-scores.IDP.wide <- dcast(scores.IDP, operation  ~ newname, value.var = "scaled.score", sum)
-row.names(scores.IDP.wide ) <- scores.IDP.wide$operation
-scores.IDP.wide$operation <- NULL
+  ## go from long to wide
+  scores.this.wide <- dcast(scores.this, operation  ~ newname, value.var = "scaled.score", sum)
+
+  ## put operation name as row.names
+  row.names(scores.this.wide ) <- scores.this.wide$operation
+  scores.this.wide$operation <- NULL
+
+  ## Compile the score for the theme
+  themes.op[ , i + 1] <- rowSums(scores.this.wide, na.rm = TRUE, dims = 1)  / ncol(scores.this.wide)
+  ## sclae it
+  #themes.op[ , i + 1] <- round(scale(themes.op[ , i + 1],
+  #                             center = min(themes.op[ , i + 1]),
+  #                             scale = max(themes.op[ , i + 1]) - min(themes.op[ , i + 1])) , 2)
+  names(themes.op)[i + 1] <- theme.this
+
+  ## save with different name
+  assign(  paste("scores.", theme.this, sep = ""), scores.this.wide )
+  rm(scores.this, theme.this,scores.this.wide )
+
+}
+
+
+##### Step : subsetting by concept to check clusters #####
+concept.op <- as.data.frame(data$intro.Operation)
+concept <- as.data.frame(unique(questions$concept))
+for (i in 1:nrow(concept)) {
+  # i <- 2
+  concept.this <- as.character(concept[ i, 1])
+  ## subset
+  scores.this <- scores.full[ !is.na(scores.full$scaled.score) & scores.full$concept == concept.this , ]
+
+  ## go from long to wide
+  scores.this.wide <- dcast(scores.this, operation  ~ newname, value.var = "scaled.score", sum)
+
+  ## put operation name as row.names
+  row.names(scores.this.wide ) <- scores.this.wide$operation
+  scores.this.wide$operation <- NULL
+
+
+  ## Compile the score for the concept
+  concept.op[ , i + 1] <- rowSums(scores.this.wide, na.rm = TRUE, dims = 1) / ncol(scores.this.wide)
+  ## scale it
+  #concept.op[ , i + 1] <- round(scale(concept.op[ , i + 1],
+  #                                   center = min(concept.op[ , i + 1]),
+  #                                   scale = max(concept.op[ , i + 1]) - min(concept.op[ , i + 1])) , 2)
+
+
+  names(concept.op)[i + 1] <- concept.this
+
+
+  ##
+  assign(  paste("scores.", concept.this, sep = ""), scores.this.wide )
+  rm(scores.this, concept.this,scores.this.wide )
+}
+
 
 
 ## get a vector with subregion for representation
 subregion <- as.data.frame(data[order(data$intro.Operation), c("intro.SubRegion")])
 names(subregion)[1] <- "subregion"
 
-## Now perform Principal Componnent Analysis
-library("FactoMineR")
-library("factoextra")
-library("corrplot")
-library("bbplot")
-
-## PCA
-res.pca.Readiness <- PCA(scores.Readiness.wide, graph = FALSE)
-
-
-## Eigen Value
-eig.Readiness <- get_eigenvalue(res.pca.Readiness)
-## Scree Plot
-fviz_eig(res.pca.Readiness, addlabels = TRUE, ylim = c(0, 50))
-
-#  plot variables
-fviz_pca_var(res.pca.Readiness, col.var = "black")
-## Quality of respesentation
-var.Readiness <- get_pca_var(res.pca.Readiness)
-
-corrplot(var.Readiness$cos2, is.corr = FALSE)
-# Total cos2 of variables on Dim.1 and Dim.2
-fviz_cos2(res.pca.Readiness, choice = "var", axes = 1:2)
-# Color by cos2 values: quality on the factor map
-fviz_pca_var(res.pca.Readiness, col.var = "cos2",
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE # Avoid text overlapping
-)
-
-
-# Most contributing variables for each dimension:
-corrplot(var.Readiness$contrib, is.corr = FALSE)
-
-
-
-# Create a grouping variable using kmeans
-# Create 3 groups of variables (centers = 3)
-res.km.Readiness <- kmeans(var.Readiness$coord, centers = 3, nstart = 25)
-grp.Readiness <- as.factor(res.km.Readiness$cluster)
-# Color variables by groups
-fviz_pca_var(res.pca.Readiness, col.var = grp.Readiness,
-             palette = c("#0073C2FF", "#EFC000FF", "#868686FF"),
-             legend.title = "Cluster")
-
-## Graph of individuals
-# change the point size according the cos2 of the corresponding individuals:
-fviz_pca_ind(res.pca.Readiness,
-             col.ind = "cos2",
-             pointsize = "cos2",
-             pointshape = 21, fill = "#E7B800",
-             repel = TRUE # Avoid text overlapping (slow if many points)
-)
-# most important (or, contributing) variables can be highlighted on the correlation plot as follow:
-contrib <- fviz_pca_var(res.pca.Readiness,
-                        col.var = "contrib",
-                        gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
-)
-ggpubr::ggpar(contrib,
-              title = "Indicator Composition",
-              subtitle = "Readiness",
-              caption = "Diagnostic Tool",
-              #xlab = "Axe 1", ylab = "Axe 2",
-              #legend.title = "Subregion", legend.position = "top",
-              ggtheme = theme_minimal()
-)
-
-
-### Kept for Vis Readiness ############
-## PCA
-res.pca.Readiness <- PCA(scores.Readiness.wide, graph = FALSE)
-
-# Contributions of variables to PC1
-fviz_contrib(res.pca.Readiness,
-             choice = "var",
-             axes = 1,
-             top = 10)
-# Contributions of variables to PC2
-fviz_contrib(res.pca.Readiness,
-             choice = "var",
-             axes = 2,
-             top = 10)
-
-
-## clusters
-ind.p <- fviz_pca_ind(res.pca.Readiness,
-           #  geom.ind = "point", # show points only (nbut not "text")
-             col.ind = subregion$subregion, # color by groups
-             pointsize = "cos2",
-             pointshape = 21, #fill = "#E7B800",
-             palette = c("#00AFBB", "#E7B800", "#FC4E07"),
-           #  addEllipses = TRUE, # Concentration ellipses
-             legend.title = "Groups"
-        )
-
-ggpubr::ggpar(ind.p,
-              title = "Readiness Profile",
-              subtitle = "Principal Component Analysis",
-              caption = "Diagnostic Tool",
-              xlab = "Axe 1", ylab = "Axe 2",
-              legend.title = "Subregion",
-              legend.position = "right",
-              ggtheme = theme_minimal()
-)
-
-
-### Kept for Vis Access #################
-## PCA
-res.pca.Access <- PCA(scores.Access.wide, graph = FALSE)
-
-# Contributions of variables to PC1
-fviz_contrib(res.pca.Access,
-             choice = "var",
-             axes = 1,
-             top = 10)
-# Contributions of variables to PC2
-fviz_contrib(res.pca.Access,
-             choice = "var",
-             axes = 2,
-             top = 10)
-
-
-## clusters
-ind.p <- fviz_pca_ind(res.pca.Access,
-                      #  geom.ind = "point", # show points only (nbut not "text")
-                      col.ind = subregion$subregion, # color by groups
-                      pointsize = "cos2",
-                      pointshape = 21, #fill = "#E7B800",
-                      palette = c("#00AFBB", "#E7B800", "#FC4E07"),
-                      #  addEllipses = TRUE, # Concentration ellipses
-                      legend.title = "Groups"
-)
-
-ggpubr::ggpar(ind.p,
-              title = "Access Profile",
-              subtitle = "Principal Component Analysis",
-              caption = "Diagnostic Tool",
-              xlab = "Axe 1", ylab = "Axe 2",
-              legend.title = "Subregion",
-              legend.position = "right",
-              ggtheme = theme_minimal()
-)
-
-
-
-### Kept for Vis Situation ###############
-## PCA
-res.pca.Situation <- PCA(scores.Situation.wide, graph = FALSE)
-
-# Contributions of variables to PC1
-fviz_contrib(res.pca.Situation,
-             choice = "var",
-             axes = 1,
-             top = 10)
-# Contributions of variables to PC2
-fviz_contrib(res.pca.Situation,
-             choice = "var",
-             axes = 2,
-             top = 10)
-
-
-## clusters
-ind.p <- fviz_pca_ind(res.pca.Situation,
-                      #  geom.ind = "point", # show points only (nbut not "text")
-                      col.ind = subregion$subregion, # color by groups
-                      pointsize = "cos2",
-                      pointshape = 21, #fill = "#E7B800",
-                      palette = c("#00AFBB", "#E7B800", "#FC4E07"),
-                      #  addEllipses = TRUE, # Concentration ellipses
-                      legend.title = "Groups"
-)
-
-ggpubr::ggpar(ind.p,
-              title = "Situation Profile",
-              subtitle = "Principal Component Analysis",
-              caption = "Diagnostic Tool",
-              xlab = "Axe 1", ylab = "Axe 2",
-              legend.title = "Subregion",
-              legend.position = "right",
-              ggtheme = theme_minimal()
-)
-
-
-### Kept for Vis Environement ############
-## PCA
-res.pca.Environement <- PCA(scores.Environement.wide, graph = FALSE)
-
-# Contributions of variables to PC1
-fviz_contrib(res.pca.Environement,
-             choice = "var",
-             axes = 1,
-             top = 10)
-# Contributions of variables to PC2
-fviz_contrib(res.pca.Environement,
-             choice = "var",
-             axes = 2,
-             top = 10)
-
-
-## clusters
-ind.p <- fviz_pca_ind(res.pca.Environement,
-                      #  geom.ind = "point", # show points only (nbut not "text")
-                      col.ind = subregion$subregion, # color by groups
-                      pointsize = "cos2",
-                      pointshape = 21, #fill = "#E7B800",
-                      palette = c("#00AFBB", "#E7B800", "#FC4E07"),
-                      #  addEllipses = TRUE, # Concentration ellipses
-                      legend.title = "Groups"
-)
-
-ggpubr::ggpar(ind.p,
-              title = "Environement Profile",
-              subtitle = "Principal Component Analysis",
-              caption = "Diagnostic Tool",
-              xlab = "Axe 1", ylab = "Axe 2",
-              legend.title = "Subregion",
-              legend.position = "right",
-              ggtheme = theme_minimal()
-)
